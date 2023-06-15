16fb035a978e1f3a8bca755a179b0433f15c9900
DevloperHS <harshshukla2000.hs@gmail.com>
2023-06-15 14:29:02 +0530

    removed unecessary

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const isLocal = typeof process.pkg === "undefined";
 const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
 const { MODE } = require(path.join(basePath, "src/blendMode.js"));
-const description = "<your-stackup-username>'s PolyAliens NFT collection.";
+const description = "PolyAliens NFT collection."; //change name here
 const baseUri = "ipfs://UPDATE";
 
 const layerConfigurations = [
